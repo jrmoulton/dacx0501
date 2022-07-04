a676d3790e6d801899d7e35effb24fb5a40eee40
Jared Moulton <jrmoulton@protonmail.com>
2022-07-03 22:14:28 -0700

    Add working implementation

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+
+use embedded_hal::spi;
+
+/// Currently this dac requires that a device be able to write and flush a bus.
+/// The flushing capability is not currenlty being used but for future
+/// compatability we will keep it until certain that flushing is guarenteed to
+/// be unnecessary.
+pub struct Dac80501<Spi>
+where
+    Spi: spi::blocking::SpiBusWrite + spi::blocking::SpiBusFlush,
+{
+    spi: Spi,
+    data: [u8; 3],
+}
+
+impl<Spi> Dac80501<Spi>
+where
+    Spi: spi::blocking::SpiBusWrite + spi::blocking::SpiBusFlush,
+{
+    pub fn new(spi: Spi) -> Self {
+        Self {
+            spi,
+            data: [0x08u8, 0, 0],
+        }
+    }
+
+    pub fn set_output_level(&mut self, level: u16) -> Result<(), <Spi as spi::ErrorType>::Error> {
+        self.data[1..].copy_from_slice(level.to_be_bytes().as_slice());
+        self.spi.write(&self.data)?;
+        Ok(())
+    }
+}
